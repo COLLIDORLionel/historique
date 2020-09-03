2c70986a073fccf914420c21dbbae57d64e25597
lionelcollidor <lionel.collidor2016@campus-eni.fr>
2020-09-03 21:56:41 +0200

    init project commit

---
+++ src/test/java/fr/lionelcollidor/historique/HistoriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lionelcollidor.historique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
