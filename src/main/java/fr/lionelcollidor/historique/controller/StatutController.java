972f967b10c572a94d62db973472937919fcce0d
lionelcollidor <lionel.collidor2016@campus-eni.fr>
2020-09-08 00:46:49 +0200

    ajout getStatutById au StatutController.

--- src/main/java/fr/lionelcollidor/historique/controller/StatutController.java
+++ src/main/java/fr/lionelcollidor/historique/controller/StatutController.java
@@ -1,12 +1,10 @@
 package fr.lionelcollidor.historique.controller;
 
+import fr.lionelcollidor.historique.exception.NotFoundException;
 import fr.lionelcollidor.historique.model.Statut;
 import fr.lionelcollidor.historique.service.IStatutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,14 @@ public class StatutController {
     @PostMapping("/statuts")
     public Statut createStatut(@RequestBody Statut newStatut){
         return this.service.createStatut(newStatut);
+    }
+
+    @GetMapping("/statuts/{id}")
+    public Statut getStatutById(@PathVariable Long id){
+        return this.service.getStatutById(id)
+                .orElseThrow( ()-> new NotFoundException(
+                        "Le statut numéro : " + id + " est introuvable."
+                ) );
     }
 
 }
