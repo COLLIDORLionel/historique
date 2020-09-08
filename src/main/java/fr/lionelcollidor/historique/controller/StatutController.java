a6b59df15d2e91692b17a29acbf78b542051f183
lionelcollidor <lionel.collidor2016@campus-eni.fr>
2020-09-08 12:53:55 +0200

    passage restfull, creation des liens pour chaque methode.

--- src/main/java/fr/lionelcollidor/historique/controller/StatutController.java
+++ src/main/java/fr/lionelcollidor/historique/controller/StatutController.java
@@ -4,9 +4,13 @@ import fr.lionelcollidor.historique.exception.NotFoundException;
 import fr.lionelcollidor.historique.model.Statut;
 import fr.lionelcollidor.historique.service.IStatutService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StatutController {
@@ -15,31 +19,60 @@ public class StatutController {
     private IStatutService service;
 
     @GetMapping("/statuts")
-    public List<Statut> getAllStatut(){
-        return this.service.getAllStatut();
+    public CollectionModel<EntityModel<Statut>> getAllStatut(){
+        List<EntityModel<Statut>> statuts = this.service.getAllStatut()
+                .stream()
+                .map(
+                        statut -> EntityModel.of(statut,
+                                WebMvcLinkBuilder.linkTo(
+                                        WebMvcLinkBuilder.methodOn(StatutController.class).getStatutById(statut.getId())).withSelfRel(),
+                                WebMvcLinkBuilder.linkTo(
+                                        WebMvcLinkBuilder.methodOn(StatutController.class).getAllStatut()).withRel("statuts")
+                        )
+                ).collect(Collectors.toList());
+
+        return CollectionModel.of(statuts, WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(StatutController.class).getAllStatut()).withSelfRel()
+        );
     }
 
     @PostMapping("/statuts")
-    public Statut createStatut(@RequestBody Statut newStatut){
-        return this.service.createOrUpdateStatut(newStatut);
+    public EntityModel<Statut> createStatut(@RequestBody Statut newStatut){
+        Statut sCreate = this.service.createOrUpdateStatut(newStatut);
+
+        return EntityModel.of(sCreate, WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(StatutController.class).getStatutById(sCreate.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(StatutController.class).getAllStatut()).withRel("statuts")
+        );
     }
 
     @GetMapping("/statuts/{id}")
-    public Statut getStatutById(@PathVariable Long id){
-        return this.service.getStatutById(id)
+    public EntityModel<Statut> getStatutById(@PathVariable Long id){
+        Statut s = this.service.getStatutById(id)
                 .orElseThrow( ()-> new NotFoundException(
                         "Le statut numéro : " + id + " est introuvable."
                 ) );
+
+        return EntityModel.of(s, WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(StatutController.class).getStatutById(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(StatutController.class).getAllStatut()).withRel("statuts")
+        );
+
     }
 
     @PutMapping("/statuts/{id}")
-    public Statut updateStatut(@RequestBody Statut updateStatut, @PathVariable Long id){
-        return this.service.getStatutById(id)
+    public EntityModel<Statut> updateStatut(@RequestBody Statut updateStatut, @PathVariable Long id){
+        Statut sUpdate = this.service.getStatutById(id)
                 .map(statut -> {
                     statut.setNom(updateStatut.getNom());
                     return this.service.createOrUpdateStatut(statut);
                 })
                 .orElseThrow( ()-> new NotFoundException("Le statut n'existe pas."));
+
+        return EntityModel.of(sUpdate, WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(StatutController.class).getStatutById(sUpdate.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(StatutController.class).getAllStatut()).withRel("statuts")
+        );
     }
 
     @DeleteMapping("/statuts/{id}")
