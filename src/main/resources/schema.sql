8d38ef918ab415548e9ebe881ad98d8880830093
lionelcollidor <lionel.collidor2016@campus-eni.fr>
2020-09-09 18:12:19 +0200

    mise a jout du schema de creation de la bdd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,32 @@
 CREATE TABLE IF NOT EXISTS public.statut
 (
-    id integer NOT NULL,
-    nom character varying(10)  NOT NULL,
+    id bigint NOT NULL DEFAULT nextval('statut_id_seq'::regclass),
+    nom character varying(10),
     CONSTRAINT statut_pkey PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS public.tache
 (
     numero character varying(11) NOT NULL,
-    com_1N character varying(2),
-    date_1N date,
+    com_1n character varying(2),
+    date_1n date,
     CONSTRAINT tache_pkey PRIMARY KEY (numero)
 );
-CREATE TABLE IF NOT EXISTS public.tache_statut
+
+CREATE TABLE IF NOT EXCISTS public.tache_statuts
 (
-    id_tache character varying(11),
-    id_statut integer,
-    date date NOT NULL,
-    CONSTRAINT tache_statut_pkey PRIMARY KEY (id_tache, id_statut),
-    CONSTRAINT tache_fkey FOREIGN KEY (id_tache)
-        REFERENCES public.tache (numero) MATCH SIMPLE
+    id bigint NOT NULL DEFAULT nextval('tache_statuts_id_seq'::regclass),
+    date_statut date NOT NULL,
+    statut_id bigint,
+    tache_numero character varying(11),
+    CONSTRAINT tache_statuts_pkey PRIMARY KEY (id),
+    CONSTRAINT uk_tache_statut_date UNIQUE (tache_numero, statut_id, date_statut),
+    CONSTRAINT statut_fk FOREIGN KEY (statut_id)
+        REFERENCES public.statut (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT statut_fkey FOREIGN KEY (id_statut)
-        REFERENCES public.statut (id) MATCH SIMPLE
+    CONSTRAINT tache_fk FOREIGN KEY (tache_numero)
+        REFERENCES public.tache (numero) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
